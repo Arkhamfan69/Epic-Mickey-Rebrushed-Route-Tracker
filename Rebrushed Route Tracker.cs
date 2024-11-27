766db45a9eb284345320c6772d92a261021eff59
Arkham <92547626+Arkhamfan69@users.noreply.github.com>
2024-11-27 01:54:05 -0600

    Add files via upload

---
+++ Rebrushed Route Tracker.cs
@@ -0,0 +1,17 @@
+<Project Sdk="Microsoft.NET.Sdk">
+
+  <PropertyGroup>
+    <OutputType>WinExe</OutputType>
+    <TargetFramework>net8.0-windows</TargetFramework>
+    <RootNamespace>Rebrushed_Route_Tracker</RootNamespace>
+    <Nullable>enable</Nullable>
+    <UseWindowsForms>true</UseWindowsForms>
+    <ImplicitUsings>enable</ImplicitUsings>
+    <AllowUnsafeBlocks>True</AllowUnsafeBlocks>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <PackageReference Include="ProcessMemory64" Version="4.0.1-beta.20221205023454" />
+  </ItemGroup>
+
+</Project>
