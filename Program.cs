dbcceb3ddcd2bfd2aacc3f5e12187960e45059a2
Arkham <92547626+Arkhamfan69@users.noreply.github.com>
2024-11-26 21:50:52 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Windows.Forms;
+
+namespace Rebrushed_Route_Tracker
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
